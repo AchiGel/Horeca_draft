53a2c80734df558780ae21c0128e0a1ca3db7b66
AchiGel <ar.gelashvili@gmail.com>
2024-11-23 15:36:42 +0400

    add popular and trending pages content

--- src/pages/Popular.tsx
+++ src/pages/Popular.tsx
@@ -1,3 +1,21 @@
+import BlogCard from "../components/BlogCard";
+
 export default function Popular() {
-  return <div>Popular</div>;
+  return (
+    <div className="px-16 py-28">
+      <h2 className="text-5xl mb-6">Popular</h2>
+      <div className="flex justify-between mb-20">
+        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </span>
+        <button>View all</button>
+      </div>
+      <div className="grid grid-cols-3 gap-x-8 gap-y-20">
+        <BlogCard dir="vertical" />
+        <BlogCard dir="vertical" />
+        <BlogCard dir="vertical" />
+        <BlogCard dir="vertical" />
+        <BlogCard dir="vertical" />
+        <BlogCard dir="vertical" />
+      </div>
+    </div>
+  );
 }
